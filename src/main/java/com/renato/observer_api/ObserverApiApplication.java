d7a58aef872f051ea771b802f1feedcdbe544e25
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-25 00:41:00 -0300

    set up

---
+++ src/main/java/com/renato/observer_api/ObserverApiApplication.java
@@ -0,0 +1,13 @@
+package com.renato.observer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObserverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renato/observer_api/ObserverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.observer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
